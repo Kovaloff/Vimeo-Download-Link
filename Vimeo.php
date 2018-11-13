65027aaffe3625e4cf70f78f7708990f0f5c1de8
Artem Kovalov <kovaloff1@gmail.com>
2018-11-13 16:25:15 +0100

    Fix video config json

--- Vimeo.php
+++ Vimeo.php
@@ -1,11 +1,14 @@
 <?php
 
-class VideoController
+/**
+ * Class Vimeo
+ */
+class Vimeo
 {
     /**
      * @var array Vimeo video quality priority
      */
-    public $vimeoQualityPrioritet = array('1080p', '720p', '540p', '360p');
+    public $vimeoQualityPrioritet = ['1080p', '720p', '540p', '360p'];
 
     /**
      * Get direct URL to Vimeo video file
@@ -17,10 +20,11 @@ class VideoController
     {
         $result = '';
         $videoInfo = $this->getVimeoVideoInfo($url);
-        if ($videoInfo && $videoObject = $this->getVimeoQualityVideo($videoInfo->request->files))
-        {
+
+        if ($videoInfo && $videoObject = $this->getVimeoQualityVideo($videoInfo->request->files)) {
             $result = $videoObject->url;
         }
+
         return $result;
     }
 
@@ -28,12 +32,18 @@ class VideoController
      * Get Vimeo Player ,Video config object
      *
      */
-    function getConfigObjectFromHtml($string, $start, $end) {
+    public function getConfigObjectFromHtml($string, $start, $end)
+    {
         $string = ' ' . $string;
         $ini = strpos($string, $start);
-        if ($ini == 0) return '';
+
+        if ($ini == 0) {
+            return '';
+        }
+
         $ini += strlen($start);
         $len = strpos($string, $end, $ini) - $ini;
+
         return substr($string, $ini, $len);
     }
 
@@ -47,21 +57,20 @@ class VideoController
     {
         $videoInfo = null;
         $page = $this->getRemoteContent($url);
-
-        $html = $this->getConfigObjectFromHtml($page, 'clip_page_config =', 'window.can_preload');
-        $json = chop(trim($html), ';');
+        $html = $this->getConfigObjectFromHtml($page, 'clip_page_config = ', 'window.can_preload');
+        $json = substr($html, 0, strpos($html, '}};') + 2);
         $videoConfig = json_decode($json);
 
-        if (isset($videoConfig->player->config_url))
-        {
+        if (isset($videoConfig->player->config_url)) {
             $videoObj = json_decode($this->getRemoteContent($videoConfig->player->config_url));
-            if (!property_exists($videoObj, 'message'))
-            {
+            if (!property_exists($videoObj, 'message')) {
                 $videoInfo = $videoObj;
             }
         }
+
         return $videoInfo;
     }
+
     /**
      * Get vimeo video object
      *
@@ -71,36 +80,38 @@ class VideoController
     public function getVimeoQualityVideo($files)
     {
         $video = null;
-        if (count($files->progressive))
-        {
+
+        if (count($files->progressive)) {
             $this->vimeoVideoQuality = $files->progressive;
         }
-        foreach ($this->vimeoQualityPrioritet as $k => $quality)
-        {
-            if ($this->vimeoVideoQuality[$k]->quality == $quality)
-            {
+
+        foreach ($this->vimeoQualityPrioritet as $k => $quality) {
+            if ($this->vimeoVideoQuality[$k]->quality == $quality) {
                 $video = $this->vimeoVideoQuality[$k];
                 break;
             }
         }
-        if (!$video)
-        {
-            foreach (get_object_vars($this->vimeoVideoQuality) as $file)
-            {
+
+        if (!$video) {
+            foreach (get_object_vars($this->vimeoVideoQuality) as $file) {
                 $video = $file;
                 break;
             }
         }
+
         return $video;
     }
+
     /**
      * Get remote content by URL
      *
      * @param string $url remote page URL
      * @return string result content
      */
-    function getRemoteContent($url) {
+    public function getRemoteContent($url)
+    {
         $data = file_get_contents($url);
+
         return $data;
     }
 }
